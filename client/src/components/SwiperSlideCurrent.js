e0c0c531380043ad1fe1ae57c0fd3fd4882a5a34
Emilie Dagenais <emiliedagenais89@gmail.com>
2023-01-08 20:25:00 -0500

    added swiper to current trips

---
+++ client/src/components/SwiperSlideCurrent.js
@@ -0,0 +1,189 @@
+import { useContext, useEffect } from 'react';
+import { StateContext } from '../context/StateContext';
+
+//Swiper
+import { Autoplay, Pagination, Navigation } from 'swiper';
+import styles from 'styled-components';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/swiper-bundle.min.css';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+import 'swiper/css/autoplay';
+
+//MUI Button
+import * as React from 'react';
+import { styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import ButtonBase from '@mui/material/ButtonBase';
+import Typography from '@mui/material/Typography';
+import { NavLink } from 'react-router-dom';
+
+const SwiperSlidesCurrent = () => {
+  const { userData, setCurrentTrips, currentTrips } = useContext(StateContext);
+
+  const images = [
+    {
+      url: 'https://images.unsplash.com/photo-1604591182103-e330fb53a660?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTV8fHN0cmVldCUyMHBob3RvZ3JhcGh5fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
+
+      // title: currentTrips.title,
+      width: '100%',
+    },
+  ];
+
+  const ImageButton = styled(ButtonBase)(({ theme }) => ({
+    position: 'relative',
+    height: 350,
+    [theme.breakpoints.down('sm')]: {
+      width: '100% !important', // Overrides inline-style
+      height: 100,
+    },
+    '&:hover, &.Mui-focusVisible': {
+      zIndex: 1,
+      '& .MuiImageBackdrop-root': {
+        opacity: 0.15,
+      },
+      '& .MuiImageMarked-root': {
+        opacity: 0,
+      },
+      '& .MuiTypography-root': {
+        border: '4px solid currentColor',
+      },
+    },
+  }));
+
+  const ImageSrc = styled('span')({
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    top: 0,
+    bottom: 0,
+    backgroundSize: 'cover',
+    backgroundPosition: 'center 40%',
+  });
+
+  const Image = styled('span')(({ theme }) => ({
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    top: 0,
+    bottom: 0,
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    color: theme.palette.common.white,
+  }));
+
+  const ImageBackdrop = styled('span')(({ theme }) => ({
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    top: 0,
+    bottom: 0,
+    backgroundColor: theme.palette.common.black,
+    opacity: 0.4,
+    transition: theme.transitions.create('opacity'),
+  }));
+
+  const ImageMarked = styled('span')(({ theme }) => ({
+    height: 3,
+    width: 18,
+    backgroundColor: theme.palette.common.white,
+    position: 'absolute',
+    bottom: -2,
+    left: 'calc(50% - 9px)',
+    transition: theme.transitions.create('opacity'),
+  }));
+
+  useEffect(() => {
+    const getCurrentTrips = async () => {
+      const res = await fetch(`/api/currentTrips/${userData._id}`);
+      const json = await res.json();
+      // console.log(json);
+      setCurrentTrips(json.data);
+    };
+    getCurrentTrips();
+  }, []);
+
+  return (
+    <>
+      {currentTrips && (
+        <Swiper
+          spaceBetween={30}
+          centeredSlides={true}
+          autoplay={{
+            delay: 2500,
+            disableOnInteraction: false,
+          }}
+          pagination={{
+            clickable: true,
+          }}
+          navigation={true}
+          modules={[Autoplay, Pagination, Navigation]}
+          className='mySwiper'
+          style={{ width: '49%', height: '60%' }}
+        >
+          {currentTrips.map((trip) => {
+            return (
+              <SwiperSlide>
+                <StyledContainer>
+                  <Box
+                    sx={{
+                      display: 'flex',
+                      flexWrap: 'wrap',
+                      minWidth: 300,
+                      width: '100%',
+                    }}
+                  >
+                    {images.map((image) => (
+                      <ImageButton
+                        focusRipple
+                        key={image.title}
+                        style={{
+                          width: image.width,
+                        }}
+                      >
+                        <ImageSrc
+                          style={{ backgroundImage: `url(${image.url})` }}
+                        />
+                        <ImageBackdrop className='MuiImageBackdrop-root' />
+                        <Image>
+                          <Typography
+                            component='span'
+                            variant='subtitle1'
+                            color='inherit'
+                            sx={{
+                              position: 'relative',
+                              p: 4,
+                              pt: 2,
+                              pb: (theme) => `calc(${theme.spacing(1)} + 6px)`,
+                            }}
+                          >
+                            <StyledNavLink to={`/details/${trip._id}`}>
+                              <p>{trip.title}</p>
+                              <p>Duration: {trip.days} Day(s)</p>
+                            </StyledNavLink>
+                            <ImageMarked className='MuiImageMarked-root' />
+                          </Typography>
+                        </Image>
+                      </ImageButton>
+                    ))}
+                  </Box>
+                </StyledContainer>
+              </SwiperSlide>
+            );
+          })}
+        </Swiper>
+      )}
+    </>
+  );
+};
+
+const StyledContainer = styles.div``;
+const StyledNavLink = styles(NavLink)`
+text-decoration:none;
+color: white;
+`;
+export default SwiperSlidesCurrent;
--- client/src/pages/CurrentTrip.js
+++ client/src/pages/CurrentTrip.js
@@ -1,7 +1,7 @@
 import { useContext, useEffect } from 'react';
 import { StateContext } from '../context/StateContext';
-import { NavLink } from 'react-router-dom';
 import styled from 'styled-components';
+import SwiperSlidesCurrent from '../components/SwiperSlideCurrent';
 
 const CurrentTrip = () => {
   const { userData, setCurrentTrips, currentTrips } = useContext(StateContext);
@@ -10,7 +10,6 @@ const CurrentTrip = () => {
     const getCurrentTrips = async () => {
       const res = await fetch(`/api/currentTrips/${userData._id}`);
       const json = await res.json();
-      // console.log(json);
       setCurrentTrips(json.data);
     };
     getCurrentTrips();
@@ -22,21 +21,7 @@ const CurrentTrip = () => {
         <StyledCurrentTrips>
           <StyledHead>List of Adventures</StyledHead>
           <StyledEx>Click on a Trip to View/Edit</StyledEx>
-          <StyledContainer>
-            {currentTrips.map((trip, index) => {
-              console.log(trip);
-              return (
-                <StyledNav to={`/details/${trip._id}`}>
-                  <StyledTripContainer key={trip.index}>
-                    <StyledTripName>{trip.title}</StyledTripName>
-                    <StyledTripDetails>
-                      Duration: {trip.days} Day(s){' '}
-                    </StyledTripDetails>
-                  </StyledTripContainer>
-                </StyledNav>
-              );
-            })}
-          </StyledContainer>
+          <SwiperSlidesCurrent />
         </StyledCurrentTrips>
       )}
     </>
@@ -54,49 +39,6 @@ const StyledHead = styled.h2`
   text-align: center;
   font-size: 30px;
   padding: 80px 10px 10px 10px;
-`;
-
-const StyledContainer = styled.div`
-  margin-top: 50px;
-  display: flex;
-  gap: 16px;
-  /* border: 2px solid pink; */
-  display: flex;
-  justify-content: space-evenly;
-`;
-
-const StyledTripContainer = styled.div`
-  /* border: 3px solid purple; */
-  background-color: #d6ccc2;
-  border-radius: 10px;
-  box-shadow: rgba(50, 50, 93, 0.25) 0px 13px 27px -5px,
-    rgba(0, 0, 0, 0.3) 0px 8px 16px -8px;
-  text-align: center;
-  padding: 20px;
-  /* height: 30vh; */
-  height: 400px;
-  width: 400px;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  gap: 15px;
-`;
-
-const StyledNav = styled(NavLink)`
-  text-decoration: none;
-  color: black;
-`;
-
-const StyledTripName = styled.h3`
-  text-align: center;
-  font-size: 30px;
-  padding: 10px;
-  text-align: center;
-`;
-
-const StyledTripDetails = styled.p`
-  font-size: 20px;
 `;
 
 const StyledEx = styled.p`
