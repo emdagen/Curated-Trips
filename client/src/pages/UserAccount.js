f97975e29823c32e9964a77303371ac3e5f66cde
Emilie Dagenais <emiliedagenais89@gmail.com>
2024-05-02 20:48:42 -0400

    test

--- client/src/pages/UserAccount.js
+++ client/src/pages/UserAccount.js
@@ -24,6 +24,7 @@ const StyledContainer = styled.div`
   min-height: 50vh;
   box-shadow: rgba(50, 50, 93, 0.25) 0px 2px 5px -1px,
     rgba(0, 0, 0, 0.3) 0px 1px 3px -1px;
+  background-color: gray;
 `;
 const StyledUser = styled.div`
   display: flex;
